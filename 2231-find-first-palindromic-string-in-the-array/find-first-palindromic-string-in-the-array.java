e112c4f4687d12fbb2e6ac02fb3e9a5de7a44bf9
Sejal <sejal.sharma199x@gmail.com>
2026-07-28 19:44:02 +0530

    Time: 2 ms (58.41%) | Memory: 47.6 MB (30.41%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        String s = "";
+        //visit every word in array first
+        for(int i = 0 ; i<words.length ; i++) {
+            boolean flag = true;
+            int l = 0 , r = words[i].length()-1;
+            while(l<r) {
+             //   boolean flag = true; no not here flag should belong to current word not current iteration of while loop
+                if(words[i].charAt(l)!= words[i].charAt(r) ){
+                    //break will exit only current word and go to new i in for loop
+                        flag = false;
+                        break;
+                        
+                //    return false;
+                }
+                l++;
+                r--;
+
+            }
+            //after while loop finishes now ask if flag is still true, so return words[i]
+            if(flag) {
+                return words[i];
+            }
+        }
+        //if none of the word is a pallindrome
+        return "";
+        
+    }
+}
