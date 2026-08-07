0487096e27e832ec52525feca82a3ddec8f5476e
Sejal <sejal.sharma199x@gmail.com>
2026-08-07 22:30:20 +0530

    Update find-first-palindromic-string-in-the-array.java

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -28,3 +28,26 @@ class Solution {
         
     }
 }
+
+/* 
+class Solution {
+    public String firstPalindrome(String[] words) {
+        
+        for(int i = 0 ; i <words.length ; i++) {
+            if(palindrome(words[i])){
+                return words[i];
+            }
+            
+        }
+        return "";
+        
+    }
+    public boolean palindrome(String s) {
+        for(int i = 0 ; i <s.length()/2 ; i++) {
+            if(s.charAt(i)!= s.charAt(s.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+} */
